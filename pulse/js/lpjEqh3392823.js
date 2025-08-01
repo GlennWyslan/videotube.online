b08db22fd02759cc0671483ee21c47e4501e5666
LessyDigital <106035220+GlennWyslan@users.noreply.github.com>
2025-08-01 16:31:52 -0300

    Add files via upload

---
+++ pulse/js/lpjEqh3392823.js
@@ -0,0 +1,150 @@
+
+      function runDelayedFunctions(data) {
+        try {
+          document.querySelectorAll('.atomicat-delay').forEach(el => el.classList.remove('atomicat-delay'));
+          if(data?.setDisplayed){
+            localStorage.setItem(data?.setDisplayed, true);
+          }
+          
+    var scrollElement = document.getElementById("ancora");
+    if (scrollElement) {
+      scrollElement.scrollIntoView({ behavior: 'smooth' });
+    }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    
+    (function() {
+      try {
+        document.addEventListener('DOMContentLoaded', function () {
+          document.addEventListener("keydown", function (e) {
+            e.ctrlKey && e.preventDefault();
+          }),
+          (document.onkeydown = function (e) {
+            if (123 == e.keyCode) return !1;
+          }),
+          document.addEventListener("contextmenu", (e) => e.preventDefault());
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+    
+    (function() {
+      try {
+        const accordionTitles = document.querySelectorAll(".a-ac-t");
+        accordionTitles.forEach((title, index) => {
+          title.addEventListener("click", () => {
+            title.classList.toggle("a-ac-t-active");
+            const accordionContent = title.nextElementSibling;
+            const toggleSymbol = title.querySelector(".a-ac-tg");
+            title.childNodes[1].childNodes[0].classList.toggle('atomicat-hidden');
+            title.childNodes[1].childNodes[1].classList.toggle('atomicat-hidden');
+            accordionContent.classList.toggle("a-c-inactive");
+            accordionContent.nextElementSibling.classList.toggle("atomicat-hidden");
+          });
+        });
+      } catch (error) {
+        return error;
+      }
+    })();(function() {
+          try {
+              const clickeventList = [{"compKey":"4cfd05d","misc":{"type":"button"}},{"compKey":"ba9a403","misc":{"type":"text"}},{"compKey":"01075ae","misc":{"type":"button"}},{"compKey":"90e4c62","misc":{"type":"text"}},{"compKey":"cd2ae25","misc":{"type":"button"}},{"compKey":"f2cf3d7","misc":{"type":"text"}},{"compKey":"c332c43","misc":{"type":"text"}}];
+    
+    
+              clickeventList.forEach((comp, index) => {
+                  const compKey = comp?.compKey;
+                  const eleType = comp?.misc?.type;
+                  
+                  
+                  
+                  
+                  
+                  
+              });
+    
+          } catch (error) {
+              return error;
+          }
+      })();(function() {
+          try {
+              const animationList = [{"key":"4cfd05d","type":"button"},{"key":"01075ae","type":"button"},{"key":"cd2ae25","type":"button"}];
+    
+              animationList.forEach((animationItem, index) => {
+                const { key, type } = animationItem;
+                const elementClass = type === "container" ? ".atomicat-container-" + key : ".atomicat-element-container-" + key;
+                const targetElement = document.querySelector(elementClass);
+
+
+                    const observer = new IntersectionObserver(entries => {
+                    entries.forEach(entry => {
+                            if (entry.isIntersecting) {
+                                targetElement.style.opacity = 1;
+                                targetElement.classList.add('atomicat-entrance-animation-' + key);
+                            } else if(animationItem?.misc?.hideOffscreen) {
+                                targetElement.classList.remove('atomicat-entrance-animation-' + key);
+                                targetElement.style.opacity = 0;
+                            }
+                        });
+                    });
+
+                    observer.observe(targetElement);
+              });
+    
+          } catch (error) {
+              return error;
+          }
+      })();
+          (function() {
+            try {
+              var vturbvideoId = "687c2348b59fc4bcfb966a02";
+              var SECONDS_TO_DISPLAY = 954;
+              var attempts = 0;
+              var elsDisplayed = false;
+              var alreadyDisplayedKey = 'alreadyElsDisplayed954';
+              var alreadyElsDisplayed = localStorage.getItem(alreadyDisplayedKey);
+
+              var showHiddenElements = function () {
+                elsDisplayed = true;
+                runDelayedFunctions();
+                localStorage.setItem(alreadyDisplayedKey, true);
+              };
+              if (alreadyElsDisplayed === 'true') {
+                setTimeout(function () {
+                  showHiddenElements();
+                }, 100);
+              } else {
+                startWatchVideoProgress();
+              }
+              function getVideoInstance() {
+                if (smartplayer.instances.length > 1) {
+                  return smartplayer.instances.find(
+                    (instance) => instance.options.id === vturbvideoId
+                  );
+                }
+                return smartplayer.instances[0];
+              };
+              function startWatchVideoProgress() {
+                if (typeof smartplayer === 'undefined' || !(smartplayer.instances && smartplayer.instances.length)) {
+                  if (attempts >= 10) return;
+                  attempts += 1;
+                  return setTimeout(function () {
+                    startWatchVideoProgress();
+                  }, 1000);
+                }
+                console.log(smartplayer.instances);
+                var videoInstance = getVideoInstance();
+                videoInstance.on('timeupdate', () => {
+                  if (elsDisplayed || videoInstance.smartAutoPlay) return;
+                  console.log("currentTime => " +videoInstance.video.currentTime +" SECONDS_TO_DISPLAY => " +SECONDS_TO_DISPLAY);
+                  if (videoInstance.video.currentTime < SECONDS_TO_DISPLAY) return;
+                  showHiddenElements();
+                });
+              };
+            } catch (error) {
+              console.log(error);
+            }
+            
+          })();
+        
